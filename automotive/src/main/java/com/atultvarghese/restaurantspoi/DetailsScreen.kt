89298343d395fa279f48188e6b7a0b3f2af68dec
atultvarghese <atultvarghese@gmail.com>
2024-05-16 15:57:24 +0530

    added fav icon in action strip

--- automotive/src/main/java/com/atultvarghese/restaurantspoi/DetailsScreen.kt
+++ automotive/src/main/java/com/atultvarghese/restaurantspoi/DetailsScreen.kt
@@ -1,16 +1,23 @@
 package com.atultvarghese.restaurantspoi
 
+import android.graphics.Color
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
+import androidx.car.app.model.ActionStrip
+import androidx.car.app.model.CarColor
+import androidx.car.app.model.CarIcon
 import androidx.car.app.model.MessageTemplate
 import androidx.car.app.model.Pane
 import androidx.car.app.model.PaneTemplate
 import androidx.car.app.model.Row
 import androidx.car.app.model.Template
+import androidx.core.graphics.drawable.IconCompat
 import com.atultvarghese.restaurantspoi.data.PlacesRepository
 
 class DetailsScreen(carContext: CarContext, val placeId: Int) : Screen(carContext) {
+
+    private var isFavourite = true
     override fun onGetTemplate(): Template {
         val place = PlacesRepository().getPlace(placeId) ?:
         return MessageTemplate.Builder("Place Not Found")
@@ -23,6 +30,22 @@ class DetailsScreen(carContext: CarContext, val placeId: Int) : Screen(carContex
                 carContext.startCarApp(place.toIntent(CarContext.ACTION_NAVIGATE))
             }
             .build()
+
+        val actionStrip = ActionStrip.Builder().addAction(
+            Action.Builder()
+                .setIcon(
+                    CarIcon.Builder(
+                        IconCompat.createWithResource(
+                            carContext, R.drawable.smiling_face)
+                            )
+                        .setTint(if(isFavourite) CarColor.RED else CarColor.GREEN)
+                        .build()
+                )
+                .setOnClickListener {
+                    isFavourite = true
+                    invalidate()
+                }
+                .build())
 
         return PaneTemplate.Builder(
             Pane.Builder()
@@ -41,6 +64,7 @@ class DetailsScreen(carContext: CarContext, val placeId: Int) : Screen(carContex
                 )
                 .build()
         )
+            .setActionStrip(actionStrip.build())
             .setHeaderAction(Action.BACK)
             .build()
     }
